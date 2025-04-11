94e8711f276da2fcf3154e532fdd4c582ba91e40
Xiaoxiao <15779212418@qq.com>
2025-04-11 15:40:52 +0800

    filter 2.0

--- src/chemistry_os/src/main.py
+++ src/chemistry_os/src/main.py
@@ -11,42 +11,36 @@ from facilities.facility_bath import Bath
 from facilities.facility_filter import Filter
 
 if __name__ == '__main__':
-
-    # # fr3 = Fr3Arm("fr3","192.168.60.2")
-    
-    # fr5_A = Fr5Arm("fr5A","192.168.58.2")
-    # # # fr5_B = Fr5Arm("fr5A","192.168.59.6")
-    # # fr5_A = FacilityTemp("fr5A",0,1)
-    # # fr5_B = FacilityTemp("fr5B",2,3)
-    # # fr5_C = FacilityTemp("fr5C",4,5)
-    # main_sys = System("os")
-
-    # main_parser = CommandParser()
-    # main_parser.parse("os check")
-    # main_parser.parse("os project name=pro1 file=test2.json")
-    # main_parser.start()
-    # # main_parser.parse("fr5A out")
-    # # main_parser.parse("fr3 reset")
-    # main_parser.parse("fr5A reset")
-    
-    
-
-    #脱机测试
-    # fr5_A = FacilityTemp("fr5A",0,1)
-    # fr5_B = FacilityTemp("fr5B",2,3)
-    # fr5_C = FacilityTemp("fr5C",4,5)
-    
-    # main_bath = Bath("bath",'/dev/ttyUSB0')
     filter = Filter("filter","/dev/ttyUSB0")
     main_sys = System("os")
 
-
+    time.sleep(5)
     main_parser = CommandParser()
     # main_parser.parse("os project name=pro1 file=double1.json")
     # main_parser.parse("os project name=pro2 file=double1.json")
     main_parser.parse("os check")
-    main_parser.parse("filter test")
     main_parser.start()
+    # 气泵测试
+    main_parser.parse("filter valve state=1")
+    time.sleep(1)
+    main_parser.parse("filter airpump state=1")
+    time.sleep(40)
+    main_parser.parse("filter airpump state=0")
+    time.sleep(1)
+    main_parser.parse("filter pump state=0")
+
+    
+    # main_parser.parse("filter valve state=0")
+    # time.sleep(1)
+    # main_parser.parse("filter speed speed=600")
+    # time.sleep(1)
+    # main_parser.parse("filter dir direction=1")
+    # time.sleep(1)
+    # main_parser.parse("filter pump state=1")
+    # time.sleep(40)
+    # main_parser.parse("filter pump state=0")
+    # main_parser.parse("filter query")
+    # time.sleep(1)
     
     # 保持主线程运行
     try:
