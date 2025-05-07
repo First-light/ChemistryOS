d45a91ee69ce34705d3520bc5dcf75679e97e13b
geekzzp <1958230983@qq.com>
2025-05-07 17:57:37 +0800

    修复了部分bug

--- src/chemistry_os/src/test_701.py
+++ src/chemistry_os/src/test_701.py
@@ -1,5 +1,6 @@
 import time
 from facilities.facility_fr5arm import Fr5Arm
+from facilities.sdk import HN_SDK
 
 # fr5_C = Fr5Arm("fr5C","192.168.60.2")
 # time.sleep(3)
@@ -15,9 +16,13 @@ from facilities.facility_fr5arm import Fr5Arm
 # input('ok?')
 # fr5_C.move_to_desc([330.0,-195.0,200.0,90.0,-45.0,90.0],vel=10)
 
+hn_sdk=HN_SDK()
+hn_sdk.bath_init()
+exit()
+
+
 
 fr5_A = Fr5Arm("fr5A","192.168.58.2")
-# fr5_C = Fr5Arm("fr5C","192.168.60.2")
 # fr5_A.gripper_activate()
 # fr5_A.Go_to_start_zone_0()
 fr5_A.set_nowplace(4)
