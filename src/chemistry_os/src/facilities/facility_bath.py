0d673f2664a440517f08956bf9604e2ed8dc2de0
geekzzp <1958230983@qq.com>
2025-05-15 13:43:10 +0800

    优化bath代码封装

--- src/chemistry_os/src/facilities/facility_bath.py
+++ src/chemistry_os/src/facilities/facility_bath.py
@@ -1,4 +1,6 @@
 import sys
+from enum import Enum
+
 sys.path.append('src/chemistry_os/src')
 from facility import Facility
 from structs import FacilityState
@@ -9,16 +11,19 @@ import time
 import threading
 import sys
 import select
+from pymodbus.client import ModbusSerialClient
+
 
 class Bath(Facility):
     type = "bath"
-    public_param_1 = 0
-    public_param_2 = 1
     bath_com = '/dev/ttyUSB0'
+    bath_addr = 0x11
 
     def __init__(self,name:str):
         super().__init__(name,self.type)
-        
+        self.modbus_client = ModbusSerialClient(port=self.bath_com, baudrate=9600)
+        # self.modbus_client.connect()
+        # 连接期间会独占串口设备
 
     def output(self,param1,param2):
         print("output:",param1,param2)
@@ -57,377 +62,227 @@ class Bath(Facility):
         user_input = input("Please enter any character: ")
         print("You entered:", user_input)
 
-    def check_status(self):
+    class ControlBit(Enum):
+        """
+        水浴锅控制寄存器各位的定义和位位置。
+        根据提供的表格，定义了从第0位到第8位的控制功能。
+        第9位到第15位为预留或未使用。
+        """
+        BUZZER_MUTE =    0x0001  # 第0位: 蜂鸣器/消音键 (BCK)
+        POWER =          0x0002  # 第1位: 电源键 (PWR)
+        CHANNEL_SWITCH = 0x0004  # 第2位: 主控与辅控温度通道切换键 (RSK)
+        HEATING =        0x0008  # 第3位: 加热键 (HTK)
+        COOLING =        0x0010  # 第4位: 制冷键 (CLK)
+        CIRCULATION =    0x0020  # 第5位: 循环键 (LPK)
+        STIRRING =       0x0040  # 第6位: 搅拌键 (SRK)
+        LIQUID_ADDING =  0x0080  # 第7位: 加液键 (ALK)
+        AUTO_TUNING =    0x0100  # 第8位: 温度自整定开关 (AT)
+
+    def get_control_status(self, close_serial=True) -> int:
         """
         此为水浴锅状态查询函数
         接受字符串作为查询的动作类型
         返回查询结果
         """
-        ERROR=-1
-        pd=False
-        plist = list(list_ports.comports())
-        for port in plist:
-            if self.bath_com in port.device:
-                pd=True
-                break
-        if pd==False:
-            print("水浴端口未找到！")
-            return ERROR
-        command = bytearray([0x01, 0x03, 0x00, 0x07, 0x00, 0x01, 0x35, 0xCB])
+        ERROR = -1
+
         try:
-            with serial.Serial(self.bath_com, 9600, timeout=1) as ser:
-                ser.write(command)
-                time.sleep(0.1)
-                response = ser.read(ser.in_waiting)
-                res = int.from_bytes(response[3:5], byteorder='big')
-                print(response)
-                return res
-                
-        except Exception as e:
-            print("Error1:", str(e))
-        
-    def cold_ctr(self, on):
+            if not self.modbus_client.connect():
+                return ERROR
+
+            result = self.modbus_client.read_holding_registers(7, slave=self.bath_addr)
+
+            if result.isError():
+                return ERROR
+
+            return result.registers[0]
+        finally:
+            if close_serial:
+                self.modbus_client.close()
+
+    def _common_ctr(self, on, control_bit:ControlBit, debug_str, close_serial):
+        ERROR = -1
+
+        on = bool(on)
+        try:
+            if not self.modbus_client.connect():
+                return ERROR
+
+            status = self.get_control_status(close_serial=False)
+            if status == ERROR:
+                return ERROR
+
+            status_wanted = bool(status & control_bit.value)
+            if status_wanted == on:
+                print(debug_str + '状态不变')
+                return
+            elif status and not on:
+                print(debug_str + '允许关闭')
+            # elif not status and on:
+            else:
+                print(debug_str + '允许开启')
+
+            result = self.modbus_client.write_register(7, control_bit.value, slave=self.bath_addr)
+            if result.isError():
+                return ERROR
+        finally:
+            if close_serial:
+                self.modbus_client.close()
+
+    def power_ctr(self, on, close_serial=True):
+        """
+        此为电源状态控制函数
+        接受整数作为控制器的开关，on=1为开，on=0为关，若状态不变则不进行操作
+        """
+        return self._common_ctr(on,
+                                Bath.ControlBit.POWER,
+                               '开机',
+                                close_serial)
+    
+    def cold_ctr(self, on, close_serial=True):
+        """
+        此为制冷状态控制函数
+        接受整数作为控制器的开关，on=1为开，on=0为关，若状态不变则不进行操作
+        """
+        return self._common_ctr(on,
+                                Bath.ControlBit.COOLING,
+                               '制冷',
+                                close_serial)
+
+    def hot_ctr(self, on, close_serial=True):
         """
         此为加热状态控制函数
-        接受整数作为控制搅拌器的开关，on=1为开，on=0为关，若状态不变则不进行操作
+        接受整数作为控制器的开关，on=1为开，on=0为关，若状态不变则不进行操作
         """
-        ERROR=-1
-        pd=False
-        plist = list(list_ports.comports())
-        for port in plist:
-            print (port.device)
-            if self.bath_com in port.device:
-                pd=True
-                break
-        if pd==False:
-            print("水浴端口未找到！")
-            return ERROR
-        
-        # 查询搅拌器状态
-        statu = self.check_status()
-        statu_cold = (statu >> 4) & 1
-        
-        time.sleep(0.1)# 查询后提供一点延时，否则会干扰后续写操作
+        return self._common_ctr(on,
+                                Bath.ControlBit.HEATING,
+                               '加热',
+                                close_serial)
 
-        # 仅有当搅拌器状态与要求的状态不同时才进行状态转换
-        if statu_cold == 1 and on == 0:
-            print("制冷允许关闭")
-        elif statu_cold == 0 and on == 1:
-            print("制冷允许开启")
-        else:
-            print("制冷状态不变")
-            return
-        command = bytearray([0x01, 0x06, 0x00, 0x07, 0x00, 0x10, 0x39, 0xC7])
-        try:
-            with serial.Serial(self.bath_com, 9600, timeout=1) as ser:
-                print("成功连接")
-                ser.write(command)
-                time.sleep(0.1)
-                response = ser.read(ser.in_waiting)
-                print(response)
-
-        except Exception as e:
-            print("Error1:", str(e))
-
-    def hot_ctr(self, on):
-        """
-        此为加热状态控制函数
-        接受整数作为控制搅拌器的开关，on=1为开，on=0为关，若状态不变则不进行操作
-        """
-        ERROR=-1
-        pd=False
-        plist = list(list_ports.comports())
-        for port in plist:
-            print (port.device)
-            if self.bath_com in port.device:
-                pd=True
-                break
-        if pd==False:
-            print("水浴端口未找到！")
-            return ERROR
-        
-        # 查询搅拌器状态
-        statu = self.check_status()
-        statu_hot = (statu >> 3) & 1
-        
-        time.sleep(0.1)# 查询后提供一点延时，否则会干扰后续写操作
-
-        # 仅有当搅拌器状态与要求的状态不同时才进行状态转换
-        if statu_hot == 1 and on == 0:
-            print("加热允许关闭")
-        elif statu_hot == 0 and on == 1:
-            print("加热允许开启")
-        else:
-            print("加热状态不变")
-            return
-        command = bytearray([0x01, 0x06, 0x00, 0x07, 0x00, 0x08, 0x39, 0xCD])
-        try:
-            with serial.Serial(self.bath_com, 9600, timeout=1) as ser:
-                print("成功连接")
-                ser.write(command)
-                time.sleep(0.1)
-                response = ser.read(ser.in_waiting)
-                print(response)
-
-        except Exception as e:
-            print("Error1:", str(e))
-
-    def mix_ctr(self, on):
+    def mix_ctr(self, on, close_serial=True):
         """
         此为搅拌器控制函数
-        接受整数作为控制搅拌器的开关，on=1为开，on=0为关，若状态不变则不进行操作
+        接受整数作为控制器的开关，on=1为开，on=0为关，若状态不变则不进行操作
         """
-        ERROR=-1
-        pd=False
-        plist = list(list_ports.comports())
-        for port in plist:
-            if self.bath_com in port.device:
-                pd=True
-                break
-        if pd==False:
-            return ERROR
-        
-        # 查询搅拌器状态
-        statu = self.check_status()
-        statu_mix = (statu >> 6) & 1
-        
-        time.sleep(0.1)# 查询后提供一点延时，否则会干扰后续写操作
+        return self._common_ctr(on,
+                                Bath.ControlBit.STIRRING,
+                               '搅拌器',
+                                close_serial)
 
-        # 仅有当搅拌器状态与要求的状态不同时才进行状态转换
-        if statu_mix == 1 and on == 0:
-            print("搅拌器关闭")
-        elif statu_mix == 0 and on == 1:
-            print("搅拌器开启")
-        else:
-            print("搅拌器状态不变")
-            return
-        command = bytearray([0x01, 0x06, 0x00, 0x07, 0x00, 0x40, 0x39, 0xFB])
-        
-        try:
-            with serial.Serial(self.bath_com, 9600, timeout=1) as ser:
-                print("成功连接")
-                ser.write(command)
-                time.sleep(0.1)
-                response = ser.read(ser.in_waiting)
-                print(response)
-
-        except Exception as e:
-            print("Error1:", str(e))
-
-    def circle_ctr(self, on):
+    def circle_ctr(self, on, close_serial=True):
         """
         此为循环系统控制函数
-        接受整数作为控制搅拌器的开关，on=1为开，on=0为关，若状态不变则不进行操作
+        接受整数作为控制器的开关，on=1为开，on=0为关，若状态不变则不进行操作
         """
-        ERROR=-1
-        pd=False
-        plist = list(list_ports.comports())
-        for port in plist:
-            if self.bath_com in port.device:
-                pd=True
-                break
-        if pd==False:
-            return ERROR
-        
-        # 查询搅拌器状态
-        statu = self.check_status()
-        statu_cir = (statu >> 5) & 1
-        
-        time.sleep(0.1)# 查询后提供一点延时，否则会干扰后续写操作
+        return self._common_ctr(on,
+                                Bath.ControlBit.CIRCULATION,
+                               '循环系统',
+                                close_serial)
 
-        # 仅有当搅拌器状态与要求的状态不同时才进行状态转换
-        if statu_cir == 1 and on == 0:
-            print("循环系统关闭")
-        elif statu_cir == 0 and on == 1:
-            print("循环系统开启")
-        else:
-            print("循环系统状态不变")
-            return
-        command = bytearray([0x01, 0x06, 0x00, 0x07, 0x00, 0x20, 0x39, 0xD3])
-        
-        try:
-            with serial.Serial(self.bath_com, 9600, timeout=1) as ser:
-                print("成功连接")
-                ser.write(command)
-                time.sleep(0.1)
-                response = ser.read(ser.in_waiting)
-                print(response)
-
-        except Exception as e:
-            print("Error1:", str(e))
-
-    def convert_and_split_hex(self, value):
-        if value < 0:
-            value = (1 << 16) + value
-        hex_str = f"{value:04X}"
-        high_part = int(hex_str[:2], 16)
-        low_part = int(hex_str[2:], 16)
-        
-        return high_part, low_part
-
-    def crc16_modbus(self, data):
-        crc = 0xFFFF
-        for byte in data:
-            crc ^= byte
-            for _ in range(8):
-                lsb = crc & 1
-                crc >>= 1
-                if lsb:
-                    crc ^= 0xA001
-        return crc
-
-    def writetmp(self, num):
+    def write_temp(self, temp, close_serial=True):
         ERROR=-1
         OVERRANGE=-2
-        num*=10
-        if num>3000: 
+
+        temp*=10
+        temp = int(temp)
+        if temp>3000:
             print("温度过高！")
             return OVERRANGE
-        elif num<-900: 
+        if temp<-900:
             print("温度过低！")
             return OVERRANGE
-        
-        pd=False
-        plist = list(list_ports.comports())
-        for port in plist:
-            print (port.device)
-            if self.bath_com in port.device:
-                pd=True
-                break
-        if pd==False:
-            print("水浴端口未找到！")
-            return ERROR
-        
-        a, b = self.convert_and_split_hex(num)
-        buffer = [0x01, 0x06, 0x00, 0x02]
-        buffer.append(a)
-        buffer.append(b)
-        crc_result = self.crc16_modbus(buffer)
 
-        swapped = (crc_result >> 8) | ((crc_result & 0xFF) << 8)
-
-        buffer.append(swapped >> 8)
-        buffer.append(swapped & 0xFF)
-        
-        command = bytearray(buffer)
         try:
-            with serial.Serial(self.bath_com, 9600, timeout=1) as ser:
-                print("成功连接")
-                ser.write(command)
-                time.sleep(0.1)
-                response = ser.read(ser.in_waiting)
-                print(response)
+            if not self.modbus_client.connect():
+                return ERROR
 
-        except Exception as e:
-            print("Error1:", str(e))
+            result = self.modbus_client.write_register(2, temp, slave=self.bath_addr)
 
-    def read_working(self):
+            if result.isError():
+                return ERROR
+        finally:
+            if close_serial:
+                self.modbus_client.close()
+
+    def read_working(self, close_serial=True):
         """
         读取是否正在加热或制冷
         """
         ERROR=-1
-        pd=False
-        plist = list(list_ports.comports())
-        for port in plist:
-            if self.bath_com in port.device:
-                pd=True
-                break
-        if pd==False:
-            return ERROR
-        command = bytearray([0x01, 0x03, 0x00, 0x08, 0x00, 0x01, 0x05, 0xC8])
         try:
-            with serial.Serial(self.bath_com, 9600, timeout=1) as ser:
-                print("成功连接")
-                ser.write(command)
-                time.sleep(0.1)
-                response = ser.read(ser.in_waiting)
-                res = int.from_bytes(response[3:5], byteorder='big')
-                hot_working = res & 1
-                cold_working = (res >> 3) & 1
-                print(response)
-                print("是否正在加热：", hot_working)
-                print("是否正在制冷：", cold_working)
+            if not self.modbus_client.connect():
+                return ERROR
 
-        except Exception as e:
-            print("Error1:", str(e))
+            result = self.modbus_client.read_holding_registers(8, slave=self.bath_addr)
 
-    def readsettmp(self):
+            if result.isError():
+                return ERROR
+
+            status = result.registers[0]
+            status_cold = bool(status & 0x0080)
+            status_heat = bool(status & 0x0001)
+            print('是否正在加热：', status_heat)
+            print('是否正在制冷：', status_cold)
+        finally:
+            if close_serial:
+                self.modbus_client.close()
+
+    def read_temp_set(self, close_serial=True):
         """
         读取设置的工作温度
         """
-        ERROR=-1
-        pd=False
-        plist = list(list_ports.comports())
-        for port in plist:
-            if self.bath_com in port.device:
-                pd=True
-                break
-        if pd==False:
-            return ERROR
-        command = bytearray([0x01, 0x03, 0x00, 0x02, 0x00, 0x01, 0x25, 0xCA])
+        ERROR = -1
+
         try:
-            with serial.Serial(self.bath_com, 9600, timeout=1) as ser:
-                print("成功连接")
-                ser.write(command)
-                time.sleep(0.1)
-                response = ser.read(ser.in_waiting)
-                res = int.from_bytes(response[3:5], byteorder='big')
-                print(res/10)
+            if not self.modbus_client.connect():
+                return ERROR
 
-        except Exception as e:
-            print("Error1:", str(e))
+            result = self.modbus_client.read_holding_registers(2, slave=self.bath_addr)
 
-    def readtmp(self, test=0):
+            if result.isError():
+                return ERROR
+
+            return result.registers[0]
+        finally:
+            if close_serial:
+                self.modbus_client.close()
+
+    def read_temp(self, test=0, close_serial=True):
         """
         读取当前温度
         """
-        ERROR=-1
-        pd=False
-        plist = list(list_ports.comports())
-        for port in plist:
-            if self.bath_com in port.device:
-                pd=True
-                break
-        if pd==False:
-            return ERROR
-        command = bytearray([0x01, 0x03, 0x00, 0x00, 0x00, 0x01, 0x84, 0x0A])
-        try:
-            with serial.Serial(self.bath_com, 9600, timeout=1) as ser:
-                if test:
-                    print("成功连接")
-                ser.write(command)
-                time.sleep(0.2)
-                response = ser.read(ser.in_waiting)
-                while response==b'':
-                    print("!!!")
-                    ser.write(command)
-                    time.sleep(0.2)
-                    response = ser.read(ser.in_waiting)
-                res = int.from_bytes(response[3:5], byteorder='big', signed=False)
-                # 检查最高位是否为1（负数）
-                if res & 0x8000:
-                    # 如果是负数，进行二进制补码转换
-                    res = res - 0x10000
-                res/=10.0
-                if test:
-                    print(' ')
-                    print(response[3:5])
-                    print(response)
-                    print(res)
-                return res
+        ERROR = -1
 
-        except Exception as e:
-            print("Error1:", str(e))
+        try:
+            if not self.modbus_client.connect():
+                return ERROR
+
+            result = self.modbus_client.read_holding_registers(0, slave=self.bath_addr)
+
+            if result.isError():
+                return ERROR
+
+            temp = result.registers[0]
+            # 检查最高位是否为1（负数）
+            if temp & 0x8000:
+                # 如果是负数，进行二进制补码转换
+                temp -= 0x10000
+
+            temp /= 10.0
+            return temp
+        finally:
+            if close_serial:
+                self.modbus_client.close()
 
     def interactable_writetmp(self, tmp):
         # 用于控制是否继续计时的事件
         stop_event = threading.Event()
         countdown_finished_event = threading.Event()
         def wait_tmp(tmp):
-            now_tmp = self.readtmp()# 避免短时间内多次调用温度读取函数，以免读到错误数值
+            now_tmp = self.read_temp()# 避免短时间内多次调用温度读取函数，以免读到错误数值
             start_tmp = now_tmp+0.001
             start_time = time.time()
-            self.writetmp(tmp)# 设置水浴锅温度
+            self.write_temp(tmp)# 设置水浴锅温度
             print('控温中...')
             while(now_tmp > tmp+5 or now_tmp < tmp-5) and not stop_event.is_set():
                 bias = 5 if abs(tmp + 5 - start_tmp) < abs(tmp - 5 - start_tmp) else -5
@@ -438,7 +293,7 @@ class Bath(Facility):
                 finish_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()+remaining_time))
                 print(f"当前温度为：{now_tmp},未达到指定温度{tmp}附近，继续控温，预计剩余时间: {int(remaining_time)} 秒 | 预计结束时间: {finish_time}, 输入 \'q\' 以跳过", end="\r")
                 time.sleep(1)
-                now_tmp = self.readtmp()
+                now_tmp = self.read_temp()
                 continue
             if not stop_event.is_set():
                 print(f"\n到达指定温度附近，当前温度为{now_tmp}，共耗时{time.time() - start_time}秒")
@@ -462,3 +317,13 @@ class Bath(Facility):
 
         # 启动温度控制
         wait_tmp(tmp)
+
+if __name__ == '__main__':
+    bath = Bath('bath')
+
+    bath.power_ctr(1)
+    bath.hot_ctr(1)
+    bath.cold_ctr(1)
+    bath.mix_ctr(1)
+    bath.circle_ctr(1)
+    bath.write_temp(25)
